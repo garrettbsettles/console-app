09b602911e1abe78eb7693eeb380524d471a10da
garrettbsettles <garrettbsettles@gmail.com>
2020-12-05 09:55:00 -0800

    adding

---
+++ console-app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n What is your name?");
+            string name = Console.ReadLine();
+            DateTime date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
